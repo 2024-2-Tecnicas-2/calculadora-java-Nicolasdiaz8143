a7e714cdc607f0f57b771b9281bda00eb02cecb4
nosenombre <nosenombre@gmail.com>
2024-08-21 19:43:08 -0500

    se adicionó la potencia y modulo

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -26,9 +26,15 @@ public class Calculadora {
                 // Operación división:
                 respuesta = (double) numero1 / numero2;
                 break;
+            case '^':
+                // Operación potencia
+                respuesta =  Math.pow(numero1, numero2);
+                break;
+            case '%':
+                // Operación modulo
+                respuesta = numero1 % numero2;
+                break;
             
-            // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
-                
             default:
                 throw new IllegalArgumentException("Operación inválida.");
         }
@@ -46,7 +52,7 @@ public class Calculadora {
         
         // Lectura de la operación a realizar
         // TODO: DEBES ADICIONAR EL SÍMBOLO DE ^ Y % PARA CALCULAR LA POTENCIA Y EL MÓDULO
-        System.out.println("Ingrese la operación (+, -, *, /)");
+        System.out.println("Ingrese la operación (+, -, *, /, ^, %)");
         char operacion = lector.next().charAt(0);
         
         double respuesta = calcular(numero1, numero2, operacion);
